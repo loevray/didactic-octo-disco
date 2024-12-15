e86be18a62ecf96b72da98f352fc733e0c9b9b62
loevray <rei980213@gmail.com>
2024-12-15 14:31:37 +0900

    [♻️refactor]: encapsulation move
    - delete useless codes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -5,40 +6,40 @@ public class Player : MonoBehaviour
     
     [SerializeField]
     private float moveSpeed = 20f;
-    
-    private int lastDirection = 0;
+    [SerializeField]
+    private float moveStopThreshold = 15f;
+
     void Start()
     {
         
     }
     
-    void GetLastKeyDirection(){
-        if (Input.GetKeyDown(KeyCode.RightArrow)) 
-        {
-            lastDirection = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            lastDirection = -1;
-        }
-    }
-
-     void Move(int direction)
+     void Move()
     {
         /* 
         Input.GetAxis = 가속과 감속을 이용한 부드러운 입력변화
         Input.GetAxisRaw = 디지털 신호처럼 입력이 바로변화함
         Input.GetKeyDown을 이용한 움직임 = 위와 동일하게 감가속이 없음
         */
-        Vector3 moveDirection = new Vector3(direction, 0, 0);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        
+        Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        bool canMoveLeft =  transform.position.x > -Math.Abs(moveStopThreshold);
+        bool canMoveRight = transform.position.x < Math.Abs(moveStopThreshold);
+        
+        if (Input.GetKey(KeyCode.LeftArrow)&& canMoveLeft)
+        {
+            transform.position -= moveTo;
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && canMoveRight)
+        {
+            transform.position += moveTo;
+        }
     }
+    
+
     void Update()
     {
-         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetLastKeyDirection();
-            Move(lastDirection);
-        }
+            Move();
     }
 }
