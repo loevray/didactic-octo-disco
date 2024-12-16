7538ea551ee874b42be30df9f8581aeb5ea20879
murderer0107 <12345678945@naver.com>
2024-12-16 21:21:56 +0900

    [♻️refactor]: MapSpawner
     - add bossMapCount
     - OceanMap2,bossMap modifiy
     - problem: cat delete normalMap1

--- Assets/Scripts/MapSpawner.cs
+++ Assets/Scripts/MapSpawner.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MapSpawner : MonoBehaviour
 {
@@ -10,13 +11,17 @@ public class MapSpawner : MonoBehaviour
     [SerializeField] private GameObject[] mapPrefabs;
     [SerializeField] private GameObject[] bossMapPrefab;
     [SerializeField] private float moveSpeed = 15f;
-
-    public event Action OnMapSpawned; //EnemySpawner를 작동시키기 위한 액션
-
-    private float MapDeleteCount = 0;
+   
+    private float mapDeleteCount = 0;
     private Vector3 spawnPosition = new Vector3(0,0,40f);
     private float deleteThreshold = -40f;
     private Queue<GameObject> activeMaps = new Queue<GameObject>();
+
+    //추가사항
+    private bool isBossMapSpawned = false; // 보스맵 상태 관리
+    public event Action OnMapSpawned; //EnemySpawner를 작동시키기 위한 액션
+    [SerializeField] private float bossMapThreshold = 4; //보스맵 카운트(수정필요)
+    private GameObject currentBossMap; // 현재 보스 맵 추적(0,0,0)에서 멈추기위함
 
     void Start()
     {
@@ -27,14 +32,26 @@ public class MapSpawner : MonoBehaviour
     void Update()
     {
         MoveMaps();
-        bool isout = CheakIsMapOut();
-        if(isout){
-            SpawnMap(spawnPosition);
-            DeleteMap();
+
+        if (!isBossMapSpawned)
+        {
+            bool isout = CheckIsMapOut();
+            if (isout)
+            {
+                if(mapDeleteCount > bossMapThreshold)
+                {
+                    BossMapSpawn();
+                }
+                else
+                {
+                    SpawnMap(spawnPosition);
+                }
+                DeleteMap();
+            }
+            
         }
-        
-    
     }
+
     void SpawnMap(Vector3 position)
     {
         GameObject selectedNewMap = mapPrefabs[UnityEngine.Random.Range(0,mapPrefabs.Length-1)]; 
@@ -44,7 +61,8 @@ public class MapSpawner : MonoBehaviour
         //EnemySpawner를 작동시키기 위한 호출
         OnMapSpawned?.Invoke();
     }
-    bool CheakIsMapOut()
+
+    bool CheckIsMapOut()
     {
         GameObject activeMap = activeMaps.Peek();
         if(activeMap.transform.position.z < deleteThreshold )
@@ -58,6 +76,11 @@ public class MapSpawner : MonoBehaviour
     {
         foreach(GameObject map in activeMaps)
         {
+            if (map == currentBossMap && map.transform.position.z <= 0)
+            {
+                continue;
+            }
+
             map.transform.position += Vector3.back * moveSpeed * Time.deltaTime;
         }
     }
@@ -66,8 +89,22 @@ public class MapSpawner : MonoBehaviour
     {
         GameObject outMap = activeMaps.Dequeue();
         Destroy(outMap);
+        mapDeleteCount += 1; //맵카운트 +1
     }
 
+    void BossMapSpawn()
+    {
+        if(mapDeleteCount >= bossMapThreshold)
+        {
+            GameObject selecteBossMap = bossMapPrefab[UnityEngine.Random.Range(0, bossMapPrefab.Length)];
+            GameObject bossMapSpawn = Instantiate(selecteBossMap, spawnPosition, Quaternion.identity);
+            activeMaps.Enqueue(bossMapSpawn);
+
+            currentBossMap = bossMapSpawn;
+
+            isBossMapSpawned = true;
+        }
+    }
 }
 
 
