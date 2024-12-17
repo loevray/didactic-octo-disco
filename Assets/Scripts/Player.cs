16a7fa1b0033001cca5190b2940439cd117a095e
김영현 <87127340+loevray@users.noreply.github.com>
2024-12-17 14:58:08 +0900

    Feat/player health#12 (#15)
    * [✨feat]: player health, level
    
    - player health and level quantity control method => event-driven architectrue
    - sample attack button
    
    * [✨feat]: handleClick sample attack button
    
    - damage 1 to player
    - logging in console
    
    * [✨feat]: Get Exp
    
    - add sample get exp button
    - logging onLevelup, onExpIncreased
    - refactor  fomula of requiredExp for level
    
    * [♻️refactor]: add max hp
    
    - if levelup, increase max hp and increase current hp how (currentHp + maxhp - prevMaxHp)
    
    * [🐛fix]: max hp bug fix
    
    - set currentMaxHp when start game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,124 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     
-    [SerializeField]
-    private float moveSpeed = 20f;
-    [SerializeField]
-    private float moveStopThreshold = 15f;
+    [SerializeField] private float moveSpeed = 20f;
+    [SerializeField] private float moveStopThreshold = 15f;
+    [SerializeField] private int currentHealthPoint = 10;
+    [SerializeField] private int baseMaxHealthPoint = 10;
+    [SerializeField] private int level = 1;
+    [SerializeField]private int exp = 0;
+    
+    private int currentMaxHealthPoint;
+    
+    public event Action<int> OnLevelUp; //레벨업 이벤트
+    public event Action<int> OnExpIncreased; //경험치 증가 이벤트
+    public event Action<int> OnHpChanged;  // HP 변경 이벤트
+    public event Action OnPlayerDead;     // 플레이어 사망 이벤트
 
     void Start()
     {
+        OnHpChanged += UpdateHpUI;
+        OnPlayerDead += HandlePlayerDeath;
+        OnLevelUp += HandlePlayerLevelup;
+        OnExpIncreased += HandlePlayerExpIncreased;
         
+        currentMaxHealthPoint = GetMaxHpForLevel(level);
+        currentHealthPoint = currentMaxHealthPoint;
     }
     void Update()
     {
-            Move();
+        Move();
+        
     }
+    
+    void OnDestroy()
+    {
+        //플레이어 파괴시 메모리 누수 방지를 위해 이벤트 핸들러 제거
+        OnHpChanged -= UpdateHpUI;
+        OnPlayerDead -= HandlePlayerDeath;
+        OnLevelUp -= HandlePlayerLevelup;
+        OnExpIncreased -= HandlePlayerExpIncreased;
+    }
+
+    
+    private void HandlePlayerExpIncreased(int currentExp){
+        Debug.Log($"경험치 획득! 현재 경험치 : {currentExp} 남은 경험치: {GetExpThresholdForLevel(level) - currentExp}");
+    }
+    
+    private void HandlePlayerLevelup(int currentLevel){
+        Debug.Log($"레베루 업! 현재 경험치: {exp} 레벨 : {currentLevel} 필요량:{GetExpThresholdForLevel(currentLevel)}");
+    }
+    
+     private void HandlePlayerDeath()
+    {
+        Debug.Log("플레이어 사망~!");
+    }
+    
+    private void UpdateHpUI(int currentHp)
+    {
+        Debug.Log($"플레이어 HP: {currentHp}");
+    }
+    
+    public void TakeDamage(int damage){
+        currentHealthPoint -= damage;
+        
+        OnHpChanged?.Invoke(currentHealthPoint);
+        
+        if(currentHealthPoint<=0){
+            currentHealthPoint = 0;
+            OnPlayerDead?.Invoke();
+        }
+    }
+    
+    public void IncreaseExp(int quantity)
+    {
+        exp += quantity;
+        OnExpIncreased?.Invoke(exp);
+        CheckLevelUp();
+    }
+    
+    private int GetExpThresholdForLevel(int currentLevel)
+    {
+        return 10 + ((currentLevel-1) * 4); //레벨당 필요경험치 수정 필요
+    }
+    
+    private void CheckLevelUp()
+    {
+        while (exp >= GetExpThresholdForLevel(level))
+        {
+            exp -= GetExpThresholdForLevel(level);
+            LevelUp();
+        }
+    }
+    
+    private void LevelUp()
+    {
+        level++;
+
+        int prevMaxHealthPoint = currentMaxHealthPoint;
+        currentMaxHealthPoint = GetMaxHpForLevel(level);
+        
+        int healthPointIncrease = currentMaxHealthPoint - prevMaxHealthPoint;
+        IncreaseHealth(healthPointIncrease); 
+        
+        OnLevelUp?.Invoke(level); // 외부에서 넘겨준 레벨업 이벤트 핸들러 호출. ex) 능력 카드 선택창...
+    }
+    
+    void IncreaseHealth(int quantity){
+        int tempNewHealthPoint = currentHealthPoint + quantity;
+        
+        currentHealthPoint = Mathf.Clamp(tempNewHealthPoint, 0, currentMaxHealthPoint);
+    }
+
+    public void DecreaseHealth(int quantity){
+        currentHealthPoint -= quantity;
+    }
+    
+    private int GetMaxHpForLevel(int currentLevel)
+    {
+        return baseMaxHealthPoint + ((currentLevel - 1) * 4); ////레벨당 체력 몇 증가할지 고민
+    }
+
     
     void Move()
     {
